f8f282deffddd53b91294a7e7486cd4ae83f226f
2020stephane <stephanebrisse@gmail.com>
2026-04-11 10:10:45 +0200

    add tp4

--- js/tp4.js
+++ js/tp4.js
@@ -5,5 +5,9 @@
 //    AUTEUR  : Stephane Brisse
 //===========================================================
 function testDomTp4() {
-
+     const ref_h1 = document.querySelector("h1");
+     ref_h1.addEventListener("click",mafonction(ref_h1));
+}
+function mafonction(ref_h1) {
+     ref_h1.textContent = "titre modifie";
 }
