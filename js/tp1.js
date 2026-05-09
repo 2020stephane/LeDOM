a0e3e562554849b4986c6cc81771df7d6485cc23
2020stephane <stephanebrisse@gmail.com>
2026-05-09 08:59:16 +0200

    change tp1

--- js/tp1.js
+++ js/tp1.js
@@ -8,6 +8,7 @@ function testDomTp1() {
 
      const ref_1 = document.querySelector("p");
      ref_1.textContent = "paragraphe modifié";
+
 //      const ref_2 = document.querySelector("p");
 // ref_2.textContent = "paragraphe <strong>modifie</strong>";
 
@@ -15,9 +16,8 @@ function testDomTp1() {
 // ref_3.innerHTML = "paragraphe <strong>modifie</strong>";
 
 //      const ref_p = document.querySelectorAll("p");
-// ref_p[0].textContent = "paragraphe1 modifié";
 // ref_p[1].textContent = "paragraphe2 modifié";
-// ref_p[2].textContent = "paragraphe3 modifié";
+
 
 
 }
